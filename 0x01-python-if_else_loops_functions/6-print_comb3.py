874fdb7221fb91683da79895df056b83d70ee980
ifemi27 <opatolaifeoluwav@gmail.com>
2022-06-03 08:18:03 -0700

    10-add

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+for digit_1 in range(0, 10):
+
+    for digit_2 in range(digit_1 + 1, 10):
+
+        if digit_2 == 9 and digit_1 == 8:
+
+            print("{}{}".format(digit_1, digit_2))
+
+        else:
+
+            print("{}{}".format(digit_1, digit_2), end=", ")
